946006d46de2df86dc280a5c280fab728923cb17
Gabriel Rosendorf <gabriel.rosendorf@sharecare.com>
2017-03-24 08:44:46 -0400

    updated aws module to use profiles, and be less dumb about setting region removed a bunch of chef specific stuff added kubectl shell completion sourcing

--- modules/aws/init.zsh
+++ modules/aws/init.zsh
@@ -1,55 +1,31 @@
 # AWS Credentials
 AWS_BASE=~/.aws
 AWS_FILES=${AWS_BASE}/aws_files
+AWS_REGION=us-east-1
+AWS_DEFAULT_REGION=us-east-1
 
 aws_on() {
  export AWS_FULL=${1}
- export AWS_ACCOUNT=$(echo ${1} | awk -F "_" '{print $1}')
- export AWS_REGION=$(echo ${1} | awk -F "_" '{print $2}')
- export AWS_DEFAULT_REGION=${AWS_REGION}
+ export AWS_PROFILE_NAME=$(echo ${1} | awk -F "_" '{print $1}')
+ export AWS_REGION_NAME=$(echo ${1} | awk -F "_" '{print $2}')
 
- # AWS API
- export AWS_CREDENTIAL_FILE=${AWS_FILES}/credential-file_${AWS_ACCOUNT}
-
- #Chef Settings
- export CHEF_CREDENTIAL_FILE=~/.chef/config/chef-settings_${AWS_ACCOUNT}
-
- #Chef Server
- if [ -f $CHEF_CREDENTIAL_FILE ]; then
-     export CHEF_SERVER_URL=`cat ${CHEF_CREDENTIAL_FILE} | grep chef_server_url | cut -d '=' -f 2`
-     export CHEF_VALIDATOR_NAME=`cat ${CHEF_CREDENTIAL_FILE} | grep chef_validator_name | cut -d '=' -f 2`
-     export CHEF_VALIDATOR_FILE=`cat ${CHEF_CREDENTIAL_FILE} | grep chef_validator_file | cut -d '=' -f 2`
-     export CHEF_USERNAME=`cat ${CHEF_CREDENTIAL_FILE} | grep chef_username | cut -d '=' -f 2`
-     export CHEF_REPO_DIR=`cat ${CHEF_CREDENTIAL_FILE} | grep chef_repo_dir | cut -d '=' -f 2`
-     export CLIENT_KEY=`cat ${CHEF_CREDENTIAL_FILE} | grep chef_client_key | cut -d '=' -f 2`
+ if [ ! -z $AWS_REGION_NAME ]; then
+   export AWS_REGION=${AWS_REGION_NAME}
+   export AWS_DEFAULT_REGION=${AWS_REGION_NAME}
  fi
 
- # EC2 API
- export AWS_ACCESS_KEY=`cat ${AWS_CREDENTIAL_FILE} |grep aws_access_key_id |cut -d '=' -f 2`
- export AWS_SECRET_KEY=`cat ${AWS_CREDENTIAL_FILE} |grep aws_secret_access_key |cut -d '=' -f 2`
- export AWS_REGION=$(echo ${1} | awk -F "_" '{print $2}')
-
- # Gem TF_VARs
- export TF_VAR_project_aws_access_key=`cat ${AWS_CREDENTIAL_FILE} |grep aws_access_key_id |cut -d '=' -f 2`
- export TF_VAR_project_aws_secret_key=`cat ${AWS_CREDENTIAL_FILE} |grep aws_secret_access_key |cut -d '=' -f 2`
- export TF_VAR_project_dns_aws_access_key=`cat ~/.aws/aws_files/credential-file_grid-prod | grep aws_access_key_id | cut -d '=' -f 2`
- export TF_VAR_project_dns_aws_secret_key=`cat ~/.aws/aws_files/credential-file_grid-prod | grep aws_secret_access_key | cut -d '=' -f 2`
- export AWS_REGION=$(echo ${1} | awk -F "_" '{print $2}')
-
- # BOTO
- export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY}
- export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_KEY}
-
- # WHISK
- export WHISK_CONFIG=${CHEF_REPO_DIR}/config/whisk-${AWS_FULL}.yml
-}
+ if aws configure list --profile ${AWS_PROFILE_NAME} &> /dev/null; then
+   export AWS_PROFILE=${AWS_PROFILE_NAME}
+ else
+   echo "Profile not found: ${AWS_PROFILE_NAME}"
+   aws_off
+ fi
+ }
 
 aws_off() {
  unset AWS_FULL
- unset AWS_ACCOUNT
- unset AWS_CREDENTIAL_FILE
- unset AWS_ACCESS_KEY
- unset AWS_SECRET_KEY
- unset AWS_ACCESS_KEY_ID
- unset AWS_SECRET_ACCESS_KEY
+ unset AWS_PROFILE
+ unset AWS_PROFILE_NAME
+ unset AWS_REGION
+ unset AWS_DEFAULT_REGION
 }
